3a07513df9b1da7a7459efc3b6285408babb71e1
arunraj3 <arunrajgovindan2002@gmail.com>
2023-06-14 19:50:18 +0530

    Added Income expense

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 import Header from './components/Header';
 import Balance from './components/Balance'
+import IncomeExpense from './components/IncomeExpense';
 export default function App() {
   return (
     <>
      <Header/>
      <div className="container">
       <Balance/>
+     </div>
+     <div>
+      <IncomeExpense/>
      </div>
     </>
   );
